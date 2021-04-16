d45b1eff34efc2f285174e289678ada7975face9
Daria <jadarya@mail.ru>
2021-04-16 13:00:15 +0900

    before work with Restaurants

--- nuber-eats-backend/src/auth/auth.guard.ts
+++ nuber-eats-backend/src/auth/auth.guard.ts
@@ -2,17 +2,26 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AllowedRoles } from './role.decorator';
+import { Role } from 'src/auth/role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly relfector: Reflector) {}
-  canActivate(context: ExecutionContext) {
-    const role = this.relfector.get<AllowedRoles>('roles', context.getHandler());
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.relfector.get<AllowedRoles>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
     const gqlContext = GqlExecutionContext.create(context).getContext();
     const user = gqlContext['user'];
     if (!user) {
       return false;
     }
-    return true;
+    if (roles.includes('Any')) {
+      return true;
+    }
+
+    return roles.includes(user.role);
   }
 }
--- nuber-eats-backend/src/restaurants/restaurants.resolver.ts
+++ nuber-eats-backend/src/restaurants/restaurants.resolver.ts
@@ -3,8 +3,7 @@ import { AuthUser } from 'src/auth/auth-user.decorator';
 import { CreateRestaurantInput, CreateRestaurantOutput } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.enitity';
 import { RestaurantService } from './restaurants.service';
-import { User, UserRole } from 'src/users/entities/user.entity';
-import { SetMetadata } from '@nestjs/common';
+import { User } from 'src/users/entities/user.entity';
 import { Role } from 'src/auth/role.decorator';
 
 @Resolver((of) => Restaurant)
